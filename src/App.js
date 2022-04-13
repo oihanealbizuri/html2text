888e530fc0d6c4a0e48e9df95d22ea9a7a59ec87
oihanealbizuri <oihanealbizuri@gmail.com>
2022-04-13 19:14:00 +0200

    On button click html gets converted to plain text

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Intent, TextArea } from "@blueprintjs/core";
+import { Intent, TextArea, Button } from "@blueprintjs/core";
 import { Component } from "react";
 import GridLayout from "react-grid-layout";
 
@@ -10,10 +10,11 @@ class App extends Component {
 
     this.state = {
       htmlText: "",
-      plainText: null
+      plainText: ""
     };
 
     this.handleHtmlChange = this.handleHtmlChange.bind(this);
+    this.convertToPlainText = this.convertToPlainText.bind(this);
   }
 
   componentDidMount() {
@@ -22,6 +23,12 @@ class App extends Component {
 
   handleHtmlChange(event) {
     this.setState({htmlText: event.target.value});
+  }
+
+  convertToPlainText() {
+    const {htmlText} = this.state;
+    let plain = htmlText.replace(/<[^>]+>/g, '');
+    this.setState({plainText: plain});
   }
 
   render() {
@@ -49,7 +56,16 @@ class App extends Component {
                 value={this.state.htmlText}
             />
           </div>
-          <div className={"controlPanel"} key="controlPanel">control</div>
+          <div className={"controlPanel"} key="controlPanel">
+            <Button
+                className={"controllerButton"}
+                active={true}
+                minimal={true}
+                outlined={true}
+                onClick={() => this.convertToPlainText()}
+                // iconOnly={true}
+            />
+          </div>
           <div className={"plainText"} key="plainText">
             <TextArea
                 className={"textInput bp4-small"}
