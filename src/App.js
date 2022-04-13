f32e1a4cb0d4c3c462f17b4ff7c0ed16056594a8
oihanealbizuri <oihanealbizuri@gmail.com>
2022-04-13 21:23:10 +0200

    WIP: night theme

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
-import { Intent, TextArea, Button } from "@blueprintjs/core";
-import { Component } from "react";
+import { Intent, TextArea, Button, Switch } from "@blueprintjs/core";
+import React, { useState, Component } from "react";
 import GridLayout from "react-grid-layout";
 import { ReactComponent as Html2text } from "./html2text_logo.svg";
+import { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme } from './theme';
+import { GlobalStyles } from './global';
 
 
 class App extends Component {
@@ -11,16 +14,18 @@ class App extends Component {
 
     this.state = {
       htmlText: "",
-      plainText: ""
+      plainText: "",
+      nightTheme: false
     };
 
     this.handleHtmlChange = this.handleHtmlChange.bind(this);
     this.convertToPlainText = this.convertToPlainText.bind(this);
     this.clearContent = this.clearContent.bind(this);
+    this.changeTheme = this.changeTheme.bind(this);
   }
 
   componentDidMount() {
-    const {htmlText, plainText} = this.state;
+    const {htmlText, plainText, nightTheme} = this.state;
   }
 
   handleHtmlChange(event) {
@@ -40,6 +45,10 @@ class App extends Component {
     })
   }
 
+  changeTheme() {
+    this.setState({nightTheme: !this.state.nightTheme});
+  }
+
   render() {
     const layout = [
       { i: "header", x: 0, y: 0, w: 11, h: 5, static: true },
@@ -48,55 +57,71 @@ class App extends Component {
       { i: "plainText", x: 5.8, y: 5, w: 5.2, h: 32, static: true }
     ];
     return (
-        <GridLayout
-            className="mainLayout"
-            layout={layout}
-            cols={11}
-            rowHeight={5}
-            width={800}
-        >
-          <div className={"header"} key="header">
-            <Html2text className={"logo"} />
-          </div>
-          <div className={"htmlText"} key="htmlText">
-            <TextArea
-                className={"textInput bp4-small"}
-                fill={true}
-                intent={Intent.PRIMARY}
-                onChange={this.handleHtmlChange}
-                value={this.state.htmlText}
-            />
-          </div>
-          <div className={"controlPanel"} key="controlPanel">
-            <Button
-                className={"convertButton"}
-                active={true}
-                minimal={true}
-                outlined={true}
-                intent={Intent.PRIMARY}
-                onClick={() => this.convertToPlainText()}
-                icon={"circle-arrow-right"}
-            />
-            <Button
-                className={"clearButton"}
-                active={true}
-                minimal={true}
-                outlined={true}
-                intent={Intent.DANGER}
-                onClick={() => this.clearContent()}
-                icon={"trash"}
-            />
-          </div>
-          <div className={"plainText"} key="plainText">
-            <TextArea
-                className={"textInput bp4-small"}
-                readOnly={true}
-                fill={true}
-                intent={Intent.NONE}
-                value={this.state.plainText}
-            />
-          </div>
-        </GridLayout>
+        <ThemeProvider theme={this.state.nightTheme ? darkTheme : lightTheme}>
+          <>
+            <GlobalStyles />
+            <GridLayout
+                className="mainLayout"
+                layout={layout}
+                cols={11}
+                rowHeight={5}
+                width={800}
+            >
+              <div className={"header"} key="header">
+                <Html2text className={"logo"} />
+                <Switch
+                    /*{...this.state}}*/
+                    className={"dayNightSwitch"}
+                    innerLabel="day"
+                    innerLabelChecked="night"
+                    checked={this.state.nightTheme}
+                    large={true}
+                    onChange={() => this.changeTheme()}
+                />
+              </div>
+              <div className={"htmlText"} key="htmlText">
+                <TextArea
+                    className={"textInput bp4-small"}
+                    fill={true}
+                    intent={Intent.PRIMARY}
+                    onChange={this.handleHtmlChange}
+                    value={this.state.htmlText}
+                />
+              </div>
+              <div className={"controlPanel"} key="controlPanel">
+                <Button
+                    className={"convertButton"}
+                    active={true}
+                    minimal={true}
+                    outlined={true}
+                    intent={Intent.PRIMARY}
+                    onClick={() => this.convertToPlainText()}
+                    icon={"circle-arrow-right"}
+                />
+                <Button
+                    className={"clearButton"}
+                    active={true}
+                    minimal={true}
+                    outlined={true}
+                    intent={Intent.DANGER}
+                    onClick={() => this.clearContent()}
+                    icon={"trash"}
+                />
+              </div>
+              <div className={"plainText"} key="plainText">
+                <TextArea
+                    className={"textInput bp4-small"}
+                    readOnly={true}
+                    fill={true}
+                    intent={Intent.NONE}
+                    value={this.state.plainText}
+                />
+              </div>
+            </GridLayout>
+            <footer>
+            </footer>
+          </>
+        </ThemeProvider>
     );
   }
 }
