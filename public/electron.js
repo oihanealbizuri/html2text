21d09b5cb12665c9813a5f64dc427e86969673bb
oihanealbizuri <oihanealbizuri@gmail.com>
2022-04-14 15:28:23 +0200

    Configuration for app launch (preload, icons, etc)

--- public/electron.js
+++ public/electron.js
@@ -1,48 +1,76 @@
-const path = require('path');
+// Module to control the application lifecycle and the native browser window.
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const url = require("url");
 
-const { app, BrowserWindow } = require('electron');
-const isDev = require('electron-is-dev');
-
+// Create the native browser window.
 function createWindow() {
-    // Create the browser window.
-    const win = new BrowserWindow({
+    const mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
         resizable: false,
+        autoHideMenuBar: true,
+        hasShadow: true,
+        title: 'html2text',
+        roundedCorners: true,
+        icon: __dirname + '/icon.png',
         webPreferences: {
-            nodeIntegration: true,
+            preload: path.join(__dirname, "preload.js")
         },
     });
 
-    // and load the index.html of the app.
-    // win.loadFile("index.html");
-    win.loadURL(
-        isDev
-            ? 'http://localhost:3000'
-            : `file://${path.join(__dirname, '../build/index.html')}`
-    );
-    // Open the DevTools.
-    if (isDev) {
-        win.webContents.openDevTools({ mode: 'detach' });
+    // In production, set the initial browser path to the local bundle generated
+    // by the Create React App build process.
+    // In development, set it to localhost to allow live/hot-reloading.
+    const appURL = app.isPackaged
+        ? url.format({
+            pathname: path.join(__dirname, "index.html"),
+            protocol: "file:",
+            slashes: true,
+        })
+        : "http://localhost:3000";
+    mainWindow.loadURL(appURL);
+
+    // Automatically open Chrome's DevTools in development mode.
+    if (!app.isPackaged) {
+        mainWindow.webContents.openDevTools();
     }
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
+// This method will be called when Electron has finished its initialization and
+// is ready to create the browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+    createWindow();
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
+    app.on("activate", function () {
+        // On macOS it's common to re-create a window in the app when the
+        // dock icon is clicked and there are no other windows open.
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
+
+// Quit when all windows are closed, except on macOS.
+// There, it's common for applications and their menu bar to stay active until
+// the user quits  explicitly with Cmd + Q.
+app.on("window-all-closed", function () {
+    if (process.platform !== "darwin") {
         app.quit();
     }
 });
 
-app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow();
-    }
+// If your app has no need to navigate or only needs to navigate to known pages,
+// it is a good idea to limit navigation outright to that known scope,
+// disallowing any other kinds of navigation.
+const allowedNavigationDestinations = "https://html2text.com";
+app.on("web-contents-created", (event, contents) => {
+    contents.on("will-navigate", (event, navigationUrl) => {
+        const parsedUrl = new URL(navigationUrl);
+
+        if (!allowedNavigationDestinations.includes(parsedUrl.origin)) {
+            event.preventDefault();
+        }
+    });
 });
